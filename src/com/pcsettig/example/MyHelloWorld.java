be055b7b3bc33c483de88fe4837088316a5c0e68
whatayu <81509107+whatayu@users.noreply.github.com>
2021-03-28 17:30:41 +0800

    Add files via upload
    上傳

---
+++ src/com/pcsettig/example/MyHelloWorld.java
@@ -0,0 +1,63 @@
+package com.pcsettig.example;
+
+public class MyHelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		
+		char c1 = 'a';
+		System.out.println(c1);
+		
+		byte b = 12;
+		int i1 = b + 2;
+		System.out.println(i1);
+		
+		String s1 = "Today";
+		System.out.println(s1);
+		
+		int number = 1001;
+		String numberStr = "¾Ç¸¹: ";
+		String info = numberStr + number;
+		System.out.println(info);
+		
+		char c2 = '¦n';
+		System.out.println(c2);
+		
+		int num1 = 12;
+		int num2 = 24;
+		int result1 = num1 + num2;
+		System.out.println(result1);
+		
+		int c5 = 10;
+		int c6 = c5++;
+		System.out.println(c5);
+		
+		short s2 = 10;
+		s2++;
+		
+		System.out.println("1");
+		System.out.println("1");
+		
+		System.out.println("1");
+		
+		
+		System.out.println("1");
+		
+		for(int i =1;i<=10;i++) {
+			System.out.println(i);
+		}
+	}
+
+	class A {
+		
+	}
+	
+	class B {
+		
+	}
+
+	class C {
+	
+	}
+}
